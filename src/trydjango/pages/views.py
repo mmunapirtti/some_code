a78972709b28985d0ec96ce5418dd443b82a9b12
Pula <pulaajan@gmail.com>
2018-11-05 22:06:58 +0200

    firs working view

--- src/trydjango/pages/views.py
+++ src/trydjango/pages/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view(*args, **kwargs):
+    return HttpResponse('<h1> Mahtavaa </h1>')
