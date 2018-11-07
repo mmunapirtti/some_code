c7c23a98903ed5ef1aa44291bc5be122153e85d9
Pula <pulaajan@gmail.com>
2018-11-07 22:48:45 +0200

    new django with boost

---
+++ pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request):
+    return render(request, 'home.html', {})
+
+def about_view(request):
+    return render(request, 'about.html', {})
+
+def index_view(request):
+    return render(request, 'index.html', {})
